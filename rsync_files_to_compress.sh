116a196b29b561e75947fe72232067da9114d44f
Linuxkay <sccck000@gmail.com>
2020-02-21 10:31:48 +0900

    Made change of ffmpeg line.

--- rsync_files_to_compress.sh
+++ rsync_files_to_compress.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/bash
-#built 2020 Feb 13th
-
+# build 2020 Feb 13th.
 # rsync from yogurt's /tmp/video/ to this server's $HOME/Videso/process/
-rsync -havztP --remove-source-files -e "ssh -p putPortNumberHere " username@ipaddress:/tmp/video/ $HOME/Videos/process/
+rsync -havvztP --remove-source-files -e "ssh -p putPortNumberHere " UserName@IPAddress:/tmp/video/ $HOME/Videos/process/
 
 # move all original avi files to video dir keeping timestamps.
 cp -p $HOME/Videos/process/*.avi $HOME/Videos/video/
 
-# Compress avi files save all as mp4 files.
+# cd to working dir called process/
 cd $HOME/Videos/process/
 
-for i in *.avi; do ffmpeg -i "$i" -c:v libx264 -preset medium -crf 32  -an "$HOME//Videos/video_mini/${i%.*}.mp4"; done
+# Compress avi files mp4.
+sh $HOME/Workspace/ffmpeg_compreser.sh
 
-# Delete all avi files in /process dir since its finished
-rm -rf $HOME//Videos/process/*.avi
+# Clean up working dir
+rm -rf $HOME/Videos/process/*.avi
 rm -rf $HOME/Videos/process/*.h264
