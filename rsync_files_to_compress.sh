91ce73c20f6618d2bc2651764d0b907ff9028ee3
Linuxkay <sccck000@gmail.com>
2020-03-02 13:12:46 +0900

    added mv line to move mp4 files after ffmpeg_compresser.sh

--- rsync_files_to_compress.sh
+++ rsync_files_to_compress.sh
@@ -1,6 +1,6 @@
 # build 2020 Feb 13th.
 # rsync from yogurt's /tmp/video/ to this server's $HOME/Videso/process/
-rsync -havvztP --remove-source-files -e "ssh -p putPortNumberHere " UserName@IPAddress:/tmp/video/ $HOME/Videos/process/
+rsync -havvztP --remove-source-files -e "ssh -p 22 " pi@10.8.0.30:/tmp/video/ $HOME/Videos/process/
 
 # move all original avi files to video dir keeping timestamps.
 cp -p $HOME/Videos/process/*.avi $HOME/Videos/video/
@@ -8,8 +8,11 @@ cp -p $HOME/Videos/process/*.avi $HOME/Videos/video/
 # cd to working dir called process/
 cd $HOME/Videos/process/
 
-# Compress avi files mp4.
+# Compress avi files as mp4.
 sh $HOME/Workspace/ffmpeg_compreser.sh
+
+# move mp4 files to vide_mini
+mv $HOME/Videos/process/*.mp4 $HOME/Videos/video_mini/
 
 # Clean up working dir
 rm -rf $HOME/Videos/process/*.avi
