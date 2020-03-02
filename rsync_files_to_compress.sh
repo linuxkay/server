aae7febd6b3aaefbc9442d0c5b320629fa65eb97
Linuxkay <sccck000@gmail.com>
2020-03-02 13:16:27 +0900

    modified rsync line. fixed typo of line ffmpeg_compresser.sh --> ffmpeg_compressor.sh

--- rsync_files_to_compress.sh
+++ rsync_files_to_compress.sh
@@ -1,6 +1,6 @@
 # build 2020 Feb 13th.
 # rsync from yogurt's /tmp/video/ to this server's $HOME/Videso/process/
-rsync -havvztP --remove-source-files -e "ssh -p 22 " pi@10.8.0.30:/tmp/video/ $HOME/Videos/process/
+rsync -havvztP --remove-source-files -e "ssh -p PortNumber " username@IpAddress:/tmp/video/ $HOME/Videos/process/
 
 # move all original avi files to video dir keeping timestamps.
 cp -p $HOME/Videos/process/*.avi $HOME/Videos/video/
@@ -9,7 +9,7 @@ cp -p $HOME/Videos/process/*.avi $HOME/Videos/video/
 cd $HOME/Videos/process/
 
 # Compress avi files as mp4.
-sh $HOME/Workspace/ffmpeg_compreser.sh
+sh $HOME/Workspace/ffmpeg_compressor.sh
 
 # move mp4 files to vide_mini
 mv $HOME/Videos/process/*.mp4 $HOME/Videos/video_mini/
